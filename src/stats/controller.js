4a6b23286d091198ad193efaa0969077f60a0b2c
Ruben Barros <atosc08@gmail.com>
2020-06-19 17:20:52 +0200

    Tie with stats

--- src/stats/controller.js
+++ src/stats/controller.js
@@ -7,7 +7,13 @@ module.exports = (msg) => {
       if (err) throw err;
       if (raw) {
         msg.channel.send(
-          raw.wins + " W, " + raw.looses + " L, Total : " + raw.total
+          raw.wins +
+            " W, " +
+            raw.looses +
+            " L, " +
+            raw.tie +
+            " E, Total : " +
+            raw.total
         );
       } else {
         msg.channel.send("Le joueur n'a pas encore fait une partie.");
